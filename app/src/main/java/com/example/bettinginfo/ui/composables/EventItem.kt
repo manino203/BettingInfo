b59804cdc37dbb02219481c84b8e465241295118
Manino203 <maninohamran@gmail.com>
2024-10-14 00:38:03 +0200

    fix: EventItem visual bug

--- app/src/main/java/com/example/bettinginfo/ui/composables/EventItem.kt
+++ app/src/main/java/com/example/bettinginfo/ui/composables/EventItem.kt
@@ -3,8 +3,11 @@ package com.example.bettinginfo.ui.composables
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
@@ -12,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -99,22 +103,26 @@ private fun InfoRow(text: String, label: String){
 @Composable
 private fun Odds(odds: Odds){
     Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween
+        modifier = Modifier.fillMaxWidth().height(IntrinsicSize.Min),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally
+            Modifier.weight(.33f).fillMaxHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ){
             OddText(odd = odds.odd1, stringResource(id = R.string.win))
             OddText(odd = odds.oddX1, stringResource(id = R.string.win_draw))
         }
         Column(
+            Modifier.weight(.33f).fillMaxHeight(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             OddText(odd = odds.oddX, stringResource(id = R.string.draw))
             OddText(odd = odds.oddX2, stringResource(id = R.string.lose_draw))
         }
         Column(
+            Modifier.weight(.33f).fillMaxHeight(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             OddText(odd = odds.odd2, stringResource(id = R.string.lose))
@@ -137,7 +145,7 @@ private fun OddText(odd: Double?, label: String){
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 250)
 @Composable
 private fun EventItemPreview() {
     val sampleEvent = Event(
